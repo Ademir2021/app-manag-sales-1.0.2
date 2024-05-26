278620e7065e66d1c9fcae259b0669b70d30afbf
Ademir Souza de Almeida <centroserra@gmail.com>
2024-05-26 09:26:03 -0300

    up code

--- src/components/pagseguro/PagSeguroCardForm.tsx
+++ src/components/pagseguro/PagSeguroCardForm.tsx
@@ -26,75 +26,81 @@ export function PagSeguroCardForm({
 }: PropsPagSeguroCardForm) {
 
     return (
-        <div className="container-global">
-            <div className="main-global">
-                <LogoIn />
-                <strong>Cartões aceitos</strong>
-                <img src="img/folder_pagbank.jpg" alt="Cartões aceitos"></img>
-                <form className="main-global-form">
-                    <input
-                        type="hidden"
-                        name="public_key"
-                        onChange={handleChange}
-                        value={children.public_key || ""}
-                        disabled
-                    />
-                    <dd>{children.holder}</dd>
-                    <input
-                        type="text"
-                        name="holder"
-                        onChange={handleChange}
-                        value={children.holder || ""}
-                        placeholder="Nome no cartão"
-                        required
-                    />
-                    <input
-                        type="text"
-                        name="number"
-                        onChange={handleChange}
-                        value={children.number || ""}
-                        placeholder="Número no cartão"
-                        required
-                    />
-                    <input
-                        type="text"
-                        name="ex_month"
-                        onChange={handleChange}
-                        value={children.ex_month || ""}
-                        placeholder="Dia da valídade"
-                        required
-                    />
-                    <input
-                        type="text"
-                        name="ex_year"
-                        onChange={handleChange}
-                        value={children.ex_year || ""}
-                        placeholder="Ano da valídade"
-                        required
-                    />
-                    <input
-                        type="text"
-                        name="secure_code"
-                        onChange={handleChange}
-                        value={children.secure_code || ""}
-                        placeholder="Código de segurança (CVV)"
-                        required
-                    />
-                    <input
-                        type="hidden"
-                        name="encrypted"
-                        onChange={handleChange}
-                        value={children.encrypted || ""}
-                        disabled
-                    />
-                    <label>{paidSucess} {paid}</label>
-                    <label>{err !== '!' ? err : null}</label>
-                    <dd>{!URLNoteSubmit ? currencyFormat(paySale) : null}</dd>
-                    <>{!URLNoteSubmit ? <button className="btn btn-primary" onClick={handleSubmit}>Pagar</button> : null}</>
-                    <>{URLNoteSubmit ? <button onClick={() => { window.location.replace(Globais.URL_NOTE + '/' + URLNoteSubmit) }}>Emitir Nota</button> : null}</>
-                    <>{URLNoteSubmit ? <button onClick={() => { window.location.replace('dashboardefault') }}>Sair</button> : null}</>
-                </form>
+        <>
+            <div className="container-global">
+                <div className="main-global">
+                    <LogoIn />
+                    <form className="main-global-form">
+                        <hr></hr>
+                        <input
+                            type="hidden"
+                            name="public_key"
+                            onChange={handleChange}
+                            value={children.public_key || ""}
+                            disabled
+                        />
+                        <dd>{children.holder}</dd>
+                        <input
+                            type="text"
+                            name="holder"
+                            onChange={handleChange}
+                            value={children.holder || ""}
+                            placeholder="Nome no cartão"
+                            required
+                        />
+                        <input
+                            type="text"
+                            name="number"
+                            onChange={handleChange}
+                            value={children.number || ""}
+                            placeholder="Número no cartão"
+                            required
+                        />
+                        <input
+                            type="text"
+                            name="ex_month"
+                            onChange={handleChange}
+                            value={children.ex_month || ""}
+                            placeholder="Dia da valídade"
+                            required
+                        />
+                        <input
+                            type="text"
+                            name="ex_year"
+                            onChange={handleChange}
+                            value={children.ex_year || ""}
+                            placeholder="Ano da valídade"
+                            required
+                        />
+                        <input
+                            type="text"
+                            name="secure_code"
+                            onChange={handleChange}
+                            value={children.secure_code || ""}
+                            placeholder="Código de segurança (CVV)"
+                            required
+                        />
+                        <input
+                            type="hidden"
+                            name="encrypted"
+                            onChange={handleChange}
+                            value={children.encrypted || ""}
+                            disabled
+                        />
+                        <label>{paidSucess} {paid}</label>
+                        <label>{err !== '!' ? err : null}</label>
+                        <dd>{!URLNoteSubmit ? currencyFormat(paySale) : null}</dd>
+                        <>{!URLNoteSubmit ? <button className="btn btn-primary" onClick={handleSubmit}>Pagar</button> : null}</>
+                        <>{URLNoteSubmit ? <button onClick={() => { window.location.replace(Globais.URL_NOTE + '/' + URLNoteSubmit) }}>Emitir Nota</button> : null}</>
+                        <>{URLNoteSubmit ? <button onClick={() => { window.location.replace('dashboardefault') }}>Sair</button> : null}</>
+                    </form>
+                </div>
             </div>
-        </div>
+            <div className="container-global">
+                <span>Cartões aceitos</span>
+                <img src="img/card_pag_bank.png" alt="Cartões aceitos"></img>
+            </div>
+            <br></br>
+        </>
     )
 }
--- src/components/sales/InvoiceSalesForm.tsx
+++ src/components/sales/InvoiceSalesForm.tsx
@@ -1,18 +1,19 @@
 import { currencyFormat } from '../utils/currentFormat/CurrentFormat';
+import { LogoIn } from '../utils/logoIn/LogoIn';
+import { Globais } from '../globais/Globais';
 
 import '../global-module.css'
-import { LogoIn } from '../utils/logoIn/LogoIn';
 
 type TInvoiceSalesForm = {
   children: string | number | readonly string[] | undefined | any;
   handleChange: any;
   handleSubmitCard: any;
-  handleSubmit:any;
+  handleSubmit: any;
   loadItens?: any;
   alert: string
   message: string;
-  backHomeInvoice:any;
-  token:string
+  backHomeInvoice: any;
+  token: string
 }
 
 export function InvoiceSalesForm({
@@ -28,53 +29,64 @@ export function InvoiceSalesForm({
 }: TInvoiceSalesForm) {
 
   return (
-    <div className="container-global" >
-      <div className="main-global">
-        <div className='main-global-form'>
-          <LogoIn/>
-           {/* <label>{alert}</label> */}
-          {/* <>{backHomeInvoice}</> */}
-          <a href='invoice_sales'>{token}</a>
-          <hr></hr>
-          <b>Faturar Pedido</b>
-          <a href='/person_update'>{"[ Atualização de Cadastro ]"}</a>
-          <dd><b>Nome</b> {children.person.name_pers}</dd>
-          <dd><b>Telefone</b> {children.person.phone_pers}</dd>
-          <dd><b>CPF</b> {children.person.cpf_pers}</dd>
-          <dd><b>Endereço</b> {children.person.address.address_pers}</dd>
-          <dd><b>Bairro</b> {children.person.address.bairro_pers}</dd>
-          <dd><b>Cidade</b> {children.person.address.name_city}</dd>
-          <dd><b>Estado</b> {children.person.address.uf}</dd>
-          <dd><b>Cep</b> {children.person.address.num_cep}</dd>
-          <dd><b>Subtotal</b> {currencyFormat(children.tItens)}</dd>
-          <dd><b>Cupom de desconto</b> {currencyFormat(children.disc_sale)}</dd>
-          <dd><b>Total da nota</b> {currencyFormat(children.tNote)}</dd>
-          <dd><b>Valor a pagar</b> {currencyFormat(children.paySale)}</dd>
-          <hr></hr>
-          <dd>{message}</dd>
-          <input
-            type='number'
-            name="disc_sale"
-            value={currencyFormat(children.disc_sale) || ''}
-            placeholder='cupom de Desconto'
-            required
-            onChange={handleChange}
+    <>
+      <div className="container-global" >
+        <div className="main-global">
+          <div className='main-global-form'>
+            <LogoIn />
+            <hr></hr>
+            <span><b>Finalizar compra</b></span>
+            <dd><b>SubTotal</b> {currencyFormat(children.tItens)}</dd>
+            <dd><b>Valor do desconto</b> {currencyFormat(children.disc_sale)}</dd>
+            <dd><b>Total da nota</b> {currencyFormat(children.tNote)}</dd>
+            <p><b>Valor a pagar</b> {currencyFormat(children.paySale)}</p>
+            <dd>{message}</dd>
+            <input
+              type='number'
+              name="disc_sale"
+              value={currencyFormat(children.disc_sale) || ''}
+              placeholder='Valor do desconto'
+              required
+              onChange={handleChange}
             />
-          <input
-            type='text'
-            name="paySale"
-            value={currencyFormat(children.tNote)}
-            placeholder="pagamento"
-            required
-            disabled
-            onChange={handleChange}
+            <input
+              type='text'
+              name="paySale"
+              value={currencyFormat(children.tNote)}
+              placeholder="pagamento"
+              required
+              disabled
+              onChange={handleChange}
             />
-          <button className='btn btn-primary' onClick={handleSubmitCard}>Pagar com Cartão</button>
-          <button className='btn btn-primary' onClick={handleSubmit}>Pagar com PIX ou BOLETO</button>
-          <a href='###'>{'Mantenha seu cadastro atualizado'}</a>
-          <span className='load-list-itens' >{loadItens}</span>
+            <button className='btn btn-primary' onClick={handleSubmitCard}>Pagar com Cartão</button>
+            <button className='btn btn-primary' onClick={handleSubmit}>Pagar com PIX ou BOLETO</button>
+            <a href='/person_update'>{"[ Atualização de Cadastro ]"}</a><a href='invoice_sales'>{token}</a>
+            <span className='load-list-itens' >{loadItens}</span>
+          </div>
         </div>
       </div>
-    </div>
+
+      <div className="container-global" >
+        <div className="main-global">
+          <div className='main-global-form'>
+             {/* <label>{alert}</label> */}
+            <span><b>Confira seus dados</b></span>
+            <dd><b>Nome</b> {children.person.name_pers}</dd>
+            <dd><b>Telefone</b> {children.person.phone_pers}</dd>
+            <dd><b>CPF</b> {children.person.cpf_pers}</dd>
+            <dd><b>Endereço</b> {children.person.address.address_pers}</dd>
+            <dd><b>Bairro</b> {children.person.address.bairro_pers}</dd>
+            <dd><b>Cidade</b> {children.person.address.name_city}</dd>
+            <dd><b>Estado</b> {children.person.address.uf}</dd>
+            <dd><b>Cep</b> {children.person.address.num_cep}</dd>
+            <br></br>
+            <dd><b>Dados da Filial</b></dd>
+            <dd>{Globais.company + ': ' + Globais.CNPJ}</dd>
+            
+          </div>
+        </div>
+      </div>
+      <br></ br>
+    </>
   )
 }
