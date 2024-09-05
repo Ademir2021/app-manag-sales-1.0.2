e64da1332bc2303b6297780f8de5e4699d906dc4
Ademir2021 <centroserra@gmail.com>
2024-09-04 23:12:16 -0300

    refactored code

--- src/useCases/users/UsersList.tsx
+++ src/useCases/users/UsersList.tsx
@@ -2,40 +2,18 @@ import { useState, useEffect, useContext } from "react"
 import { ListUSers, PropsUsers } from "../../components/users/UserList"
 import { FormatDate } from "../../components/utils/formatDate";
 import { Dashboard } from "../dashboard/Dashboard";
-import { HandleEnsureAuth } from "../../services/HandleEnsureAuth";
 import { AuthContext } from '../../context/auth'
-import api from '../../services/api/api'
+import { postAuthHandle } from "../../services/handleService";
 
 export function UsersList() {
   
   const { user: isLogged }: any = useContext(AuthContext);
   const [users, setUsers] = useState<PropsUsers[]>([])
-  const isLoggedParams:number = isLogged[0].id
   const [tokenMessage, setTokenMessage] = useState<string>("Usuário Autenticado !")
 
   useEffect(() => {
-    const getUSers = async () => {
-      await HandleEnsureAuth()
-      const res: any | undefined = localStorage.getItem('token')
-      const token = JSON.parse(res)
-      try {
-        const headers = {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`
-        }
-        await api.post<PropsUsers[]>('users_list', isLogged, { headers })
-          .then(response => {
-            setTokenMessage("Token Válido !")
-            setUsers(response.data)
-          })
-      } catch (err) {
-        console.log("error occurred !!" + err)
-        setTokenMessage(" Erro: 401 - Token Expirado ! ")
-                await HandleEnsureAuth()
-      }
-    }
-    getUSers()
-  }, [users, isLoggedParams]);
+    postAuthHandle('users_list',setTokenMessage,setUsers,isLogged)
+  }, [users, ]);
 
   return (
     <>
