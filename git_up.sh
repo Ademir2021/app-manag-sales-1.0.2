2081a63834ba9ff91e293c164277f7928fc6870b
Ademir2021 <centroserra@gmail.com>
2024-09-30 15:31:09 -0300

    up img_itens

--- git_up.sh
+++ git_up.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 git add .
-git commit -m "refactored code"
+# git commit -m "refactored code"
+git commit -m "up img_itens"
 # git branch -M main
 # git remote add origin git@github.com:Ademir2021/app-manag-sales-1.0.2.git
 git push -u origin main
