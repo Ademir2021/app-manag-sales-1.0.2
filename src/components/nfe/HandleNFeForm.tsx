ea04129015a63ccf317d52374a2e8cb6050adf21
Ademir2021 <centroserra@gmail.com>
2024-12-20 19:09:15 -0300

    acompanhamento NFe

--- src/components/nfe/HandleNFeForm.tsx
+++ src/components/nfe/HandleNFeForm.tsx
@@ -5,12 +5,72 @@ import { Waiting } from "../utils/waiting/Waiting"
 
 type Props = {
     sales: TSaleList[]
+    findPerson: any // função findPerson
+    children: string | number | readonly string[] | undefined | any
+    handleChange: React.ChangeEventHandler<HTMLInputElement> | undefined
+    handleSubmit: any
+    handleClear:any
 }
 
-function HandleNFeForm({ sales }: Props) {
+function HandleNFeForm({ sales, findPerson, handleChange, handleSubmit, handleClear }: Props) {
 
     // const NFeStatus = <img src="img/NFe/status/autorizada.ico" alt="img NFe autorizada"></img>
 
+    const header = <> <h1 className="text-center">Acompanhamento NFe </h1>
+        <div className="container">
+            <dd>Selecione as opções desejada</dd>
+            <table>
+                <td>
+                    <tr><input
+                        type="checkbox"
+                        name='nfe_autorizada'
+                        onChange={handleChange}
+                    /> Notas autorizadas</tr>
+                    <tr><input
+                        type="checkbox"
+                        name='nfe_impressa'
+                        onChange={handleChange}
+                    /> Notas impressas</tr>
+                    <tr><input
+                        type="checkbox"
+                        name='nfe_em_aberto'
+                        onChange={handleChange}
+                    /> Notas em aberto</tr>
+                    <tr><input
+                        type="checkbox"
+                        name='nfe_cancelada'
+                        onChange={handleChange}
+                    /> Notas canceladas</tr>
+                </td>
+                <td>
+                    <tr><input
+                        type="checkbox"
+                        name='nfe_inutilizada'
+                        onChange={handleChange}
+                    /> Notas inutilizadas</tr>
+                    <tr><input
+                        type="checkbox"
+                        name='nfe_denegada'
+                        onChange={handleChange}
+                    /> Notas denegadas</tr>
+                    <tr><input
+                        type="checkbox"
+                        name='nfe_com_problema'
+                        onChange={handleChange}
+                    /> Notas com Problemas</tr>
+                    <tr><input
+                        type="checkbox"
+                        name='nfe_enviada'
+                        onChange={handleChange}
+                    /> Notas enviadas</tr>
+                </td>
+            </table>
+        </div>
+        <form className="container p-3">
+            <button onClick={handleSubmit} className="m-1 btn btn-primary">Atualizar</button>
+            <button onClick={handleClear} className="btn btn-primary">limpar</button>
+        </form>
+    </>
     const thead = <thead>
         <tr>
             <th className='text-center'>Nota</th>
@@ -30,28 +90,31 @@ function HandleNFeForm({ sales }: Props) {
     </thead>
 
     return (
-        <table className='table bg-light mt-1 container'>
-            {sales.length === 0 ? <Waiting waiting="Aguardando Notas" /> : thead}
-            <tbody>
-                {sales.map((sale: TSaleList) => (
-                    <tr key={sale.id_sale}>
-                        <th className='text-center'>{sale.id_sale}</th>
-                        <th className="text-center">{sale.id_sale}</th>
-                        <th className="text-center">{sale.fk_name_filial}</th>
-                        <td>{sale.fk_name_pers}</td>
-                        <td>{sale.fk_name_pers}</td>
-                        <td>{FormatDate(sale.created_at)}</td>
-                        <td>{'doc'}</td>
-                        <td>{currencyFormat(sale.created_at)}</td>
-                        <td>{currencyFormat(sale.total_sale)}</td>
-                        <td>{'email'}</td>
-                        <td>{'sit'}</td>
-                        <td>{'ch'}</td>
-                        <td>{'protocolo'}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+        <>
+            {header}
+            <table className='table bg-light mt-1 container'>
+                {sales.length === 0 ? <Waiting waiting="Aguardando Notas" /> : thead}
+                <tbody>
+                    {sales.map((sale: TSaleList) => (
+                        <tr key={sale.id_sale}>
+                            <th className='text-center'>{sale.id_sale}</th>
+                            <th className="text-center">{sale.id_sale}</th>
+                            <th className="text-center">{sale.fk_name_filial}</th>
+                            <td>{sale.fk_name_pers}</td>
+                            <td>{findPerson(sale.fk_name_pers)}</td>
+                            <td>{FormatDate(sale.created_at)}</td>
+                            <td>{'doc'}</td>
+                            <td>{currencyFormat(sale.created_at)}</td>
+                            <td>{currencyFormat(sale.total_sale)}</td>
+                            <td>{'email'}</td>
+                            <td>{'sit'}</td>
+                            <td>{'ch'}</td>
+                            <td>{'protocolo'}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
     )
 }
 
--- src/useCases/nfe/HandleNFe.tsx
+++ src/useCases/nfe/HandleNFe.tsx
@@ -1,27 +1,128 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { HandleNFeForm } from "../../components/nfe/HandleNFeForm";
 import { AuthContext } from '../../context/auth'
 import { postAuthHandle } from "../../services/handleService";
 import { TSaleList } from "../sales/type/TSale";
+import { TPerson } from "../persons/type/TPerson";
+
+type INFeStatus = {
+    nfe_autorizada: string
+    nfe_impressa: string
+    nfe_em_aberto: string
+    nfe_cancelada: string
+    nfe_inutilizada: string
+    nfe_denegada: string
+    nfe_com_problema: string
+    nfe_enviada: string
+}
 
 function HandleNFe() {
 
     const { user: isLogged }: any = useContext(AuthContext);
     const [sales, setSales] = useState<TSaleList[]>([]);
+    const [persons, setPersons] = useState<TPerson[]>([])
     const [tokenMessage, setTokenMessage] = useState<string>("Usuário Autenticado !")
 
+    const [sales_autorizada, setSaleAutorizada] = useState<TSaleList[]>([])
+
+    const [nfeStatus, setNFeStatus] = useState<INFeStatus>({
+        nfe_autorizada: '',
+        nfe_impressa: '',
+        nfe_em_aberto: '',
+        nfe_cancelada: '',
+        nfe_com_problema: '',
+        nfe_denegada: '',
+        nfe_enviada: '',
+        nfe_inutilizada: ''
+    })
+
+    function clearNfeStatus(){
+        const nfeStatus_: INFeStatus = {
+            nfe_autorizada: '',
+            nfe_impressa: '',
+            nfe_em_aberto: '',
+            nfe_cancelada: '',
+            nfe_com_problema: '',
+            nfe_denegada: '',
+            nfe_enviada: '',
+            nfe_inutilizada: ''
+        }
+        setNFeStatus(nfeStatus_)
+    }
+
+    const handleChange = (e: any) => {
+        const name = e.target.name
+        const value = e.target.value
+        setNFeStatus(values => ({ ...values, [name]: value }))
+    }
+
     const getSales = async () => {
-        postAuthHandle('sale_user', setTokenMessage, setSales, isLogged)
+        await postAuthHandle('sale_user', setTokenMessage, setSales, isLogged)
+        if (sales_autorizada.length == 0) {
+            for (let sale of sales)
+                if (nfeStatus.nfe_autorizada === 'on') {
+                    if (sale.fk_name_pers === 1) {
+                        sales_autorizada.push(sale)
+                    }
+                };
+            for (let sale of sales)
+                if (nfeStatus.nfe_cancelada === 'on') {
+                    if (sale.fk_name_pers === 2) {
+                        sales_autorizada.push(sale)
+                    };
+                };
+            for (let sale of sales)
+                if (nfeStatus.nfe_em_aberto === 'on') {
+                    if (sale.fk_name_pers) {
+                        sales_autorizada.push(sale)
+                    };
+                };
+        }
+        else if (sales_autorizada.length !== 0) {
+            const sales_: TSaleList[] = []
+            setSales(sales_)
+        }
+        clearNfeStatus()
     };
 
-    if (sales.length === 0) {
-        getSales()
+    const getPersons = async () => {
+        await postAuthHandle('persons_user', setTokenMessage, setPersons, isLogged)
     };
+    useEffect(() => {
+        getPersons()
+    }, [persons])
+
+    function findPerson(id: number) {
+        for (let person of persons)
+            if (person.id_person === id)
+                return person.name_pers
+    }
+
+    function handleSubmit(e: Event) {
+        e.preventDefault()
+        getSales()
+    }
+
+    function hanndleClear(e:Event){
+        e.preventDefault()
+        const sales_: TSaleList[] = []
+        clearNfeStatus()
+        setSaleAutorizada(sales_)
+    }
 
     return (
-        <HandleNFeForm
-            sales={sales}
-        />
+        <>
+            {/* <p>{JSON.stringify(nfeStatus)}</p> */}
+            <HandleNFeForm
+                sales={sales_autorizada}
+                findPerson={findPerson}
+                handleChange={handleChange}
+                handleSubmit={handleSubmit}
+                handleClear={hanndleClear}
+            >
+                {nfeStatus}
+            </HandleNFeForm>
+        </>
     )
 }
 
