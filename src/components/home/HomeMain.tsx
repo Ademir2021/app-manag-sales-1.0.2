a81482f1d6a63b5868aef178c59d6506076dc25b
Ademir2021 <centroserra@gmail.com>
2025-03-02 22:04:56 -0300

    up code

--- src/components/home/HomeMain.tsx
+++ src/components/home/HomeMain.tsx
@@ -5,6 +5,7 @@ import * as Icon from 'phosphor-react';
 import { HomeIcons } from './HomeIcons';
 
 import './css/styles.css'
+import { InvoiceSalesForm } from '../sales/InvoiceSalesForm';
 
 const HomeMain = ({ children, handleChange, handleSubmit, msg, msgFields }: PropsHomeContact) => {
 
@@ -13,10 +14,10 @@ const HomeMain = ({ children, handleChange, handleSubmit, msg, msgFields }: Prop
 
     const cards = [
         { id: 1, item: "Emissor NFe", descric: "Modelo 55 e 65", content: 'Emissão e controle da NFe' },
-        { id: 2, item: "Emissor NFCe", descric: 'EvoFácil emissor NFECe', content: "Emite a NFCe para o pequeno varejo" },
-        { id: 3, item: "Checkout de Vendas", descric: "Integração com Cartão, PIX e Boleto", content: 'Integração de sistemas e a realização de  vendas on-line, e que o cliente possa efetuar seus pagamentos de forma segura.' },
-        { id: 4, item: "API REST e Micro Serviço", descric: "Integração de seu sistema com outras tecnologias", content: 'Micros-serviços para troca de dados' },
-        { id: 5, item: "AC-Sor Sistema de Gestão", descric: "Sistema Integrado de Vendas", content: 'Automação dos processos na empresa  Vendas, Financeiro, Fluxo de caixa, estoque, emissão de notas e muito mais.' }
+        { id: 2, item: "Emissor NFCe", descric: 'EvoFácil emissor NFECe', content: "Emite a NFCe empresa do simples" },
+        { id: 3, item: "Checkout de Vendas", descric: "Cartão, PIX e Boleto", content: 'Integração de sistemas para  vendas on-line' },
+        { id: 4, item: "API REST e Micro Serviço", descric: "Comunicação entre sistemas", content: 'Micro - Serviços com estruturas json/XML' },
+        { id: 5, item: "AC-Sor Sistema de Gestão", descric: "Sistema Integrado de Vendas", content: 'Automação dos processos na empresa com,  Vendas, Financeiro, Fluxo de caixa, estoque, emissão de notas e muito mais.' }
     ]
 
     const btnSol = <button id='btn-list' onClick={() => !vText ? setVText(true) : setVText(false)}>
@@ -47,13 +48,13 @@ const HomeMain = ({ children, handleChange, handleSubmit, msg, msgFields }: Prop
         <p>Transformando suas idéias em realidade! Solicite agora mesmo um orçamento personalizado para o seu projeto de software.</p>
         <p>Junte-se a uma empresa líder que prioriza qualidade e satisfação do cliente. Com um histórico comprovado de clientes em todo o Brasil, < br /> temos soluções para:</p>
         <ul>
-            <li>Integrar NFe com o seu sistema de gestão.</li>
-            <li>Integrar NFCe para simples emissão no pequeno varejo.</li>
-            <li>Checkout cartão, PIX e boleto.</li>
-            <li>Sistema integrado para gestão ERP.</li>
-            <li>Desenvolvimento de sistemas SAAS e sites para Web.</li>
-            <li>API REST, json/XML.</li>
-            <li>Implementação de Middleware e web-token.</li>
+            <li>AC-Sor Sistemas integrado de gestão.</li>
+            <li>EvoFácil Sistema emissor de NFCe.</li>
+            <li>API REST para comunicação das vendas on-line.</li>
+            <li>Módulo integrador com NFEs.</li>
+            <li>ManageSales Sistema SAAS para gestão das vendas on-line.</li>
+            <li>APP ManageSales para Android e IOS.</li>
+            <li>MiddleWare controle de acesso e segurança.</li>
         </ul>
     </div>
 
@@ -61,13 +62,13 @@ const textTec = <div id='v-text'>
 <p>Trabalhamos com as melhores tecnologias do mercado</p>
 <p>Integração continua e Qualidade no Desenvolviemento</p>
 <ul>
-    <li>Projetos com React e React-native</li>
-    <li>Orquestração de projetos com Docker</li>
-    <li>Versionamento de projeto com Git-Hub</li>
-    <li>Modelagem de banco de dados com Postgres</li>
-    <li>Sistemas seguro com criptografia e tokerização</li>
-    <li>APIs com Typescript e Express</li>
-    <li>Componentes para integrar NF-e</li>
+    <li>Biblioteca ReactJs, Sistemas SAAS.</li>
+    <li>Framework ReactJs-native, APP Android e IOS.</li>
+    <li>Docker, container e orquestração</li>
+    <li>GitHub versionamento e compartilhamento.</li>
+    <li>PostgreSQL, banco de dados relacional</li>
+    <li>TypeScript, interface e tipagem.</li>
+    <li>JAVA, desktops e APIs.</li>
 </ul>
 </div>
 
@@ -98,11 +99,10 @@ const textTec = <div id='v-text'>
                 </main>
                 <div id='cards'>
                     <a href='#cards-in'><button className='btn btn-primary p-2 mb-3'><b>+ </b>Soluções para sua empresa</button></a>
-                    <h1 id='cards-in'>A solução que você precisa !!</h1>
-                    <h2>O cliente encontra aqui o <b>Produtos de Software</b> com à medida necessária.<br />
-                        Utilizamos métodologias ágeis de desenvolvovimento, com isto garantimos entregas rápidas e continua.</h2>
-                    <hr></hr>
-                    <dd>Escolha o card que melhor te atenda...</dd>
+                    <p id='cards-in'>A solução que você precisa !!</p>
+                    <label>O cliente encontra aqui o <b>Produtos de Software</b> com à medida necessária.
+                        Utilizamos métodologias ágeis de desenvolvovimento, com isto garantimos entregas rápidas e continua.</label>
+                  
                     {(cards.map((card: any) => (
                         <HomeCards
                             key={card.id}
