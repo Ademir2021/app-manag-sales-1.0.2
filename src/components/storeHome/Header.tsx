51a0b21ec6e07d841e2baa97d8ce6fdc3719f3e6
Ademir2021 <centroserra@gmail.com>
2025-03-08 22:06:25 -0300

    up code

--- src/components/storeHome/Header.tsx
+++ src/components/storeHome/Header.tsx
@@ -4,21 +4,24 @@ import './css/header.css'
 
 type Props = {
     counter: number | string;
-    subtotal: number | string;
+    subtotal: number | string
 }
 
 export function Header(props: Props) {
     return (
         <div className='header-home'>
-            <a href='pe' > <strong className='header-home-carrinho' >
+            <a href='pe' > <b
+            className='header-home-carrinho' >
                 {props.counter}
                 <img alt='Carrinho'
                     src="img/carrinho_counter.png">
-                </img></strong></a>
-            <a className='header-home-sub-total'>
-                {props.subtotal}</a>
-            {<a href={"/contact"} className='header-contact'>Fale Conosco {Globais.phone}</a>}
-            <div id='header-frete'>Frete grátis</div>
+                </img></b></a>
+            {props.subtotal && <a className='header-home-sub-total'>
+                {props.subtotal}</a>}
+            {<a href={"/contact"}
+            className='header-contact'>
+                {Globais.phone}</a>}
+            {!props.subtotal &&  <div id='header-frete'>Aqui tem frete grátis</div>}
         </div>
     )
 }
